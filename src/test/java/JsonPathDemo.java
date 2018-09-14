84c68069a2b929facd63510a150f7eca3523954a
Amysheng123 <36951533+Amysheng123@users.noreply.github.com>
2018-09-14 09:41:45 +0800

    Update JsonPathDemo.java

--- src/test/java/JsonPathDemo.java
+++ src/test/java/JsonPathDemo.java
@@ -71,7 +71,7 @@ public class JsonPathDemo {
 
         //10.2返回name等于王五的值
         List<Map<String,String>> record3 = context.read("$.result.records[?@.name == '王五']");
-        System.out.println(record3+"haha");
+        System.out.println(record3+"dudu");
 
         //11 返回有test属性的数组
         List<Map<String, String>> records1 = context.read("$.result.records[?(@.test)]");
